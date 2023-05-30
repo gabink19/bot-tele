455bb651e96f046383cd2af2d6371e49968af04f
Mochamad Gibran Meidiyan <mochamad.meidiyan@spesolution.com>
2023-05-30 15:27:18 +0700

    fix

--- app/Helpers/Command.php
+++ app/Helpers/Command.php
@@ -1126,8 +1126,7 @@ class Command
         $url        = "https://api-js.prod.companyreview.co/companies/1284069/company-reviews?page=1&sort=";
         $ch         = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_POST, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','X-Country-Iso: id'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result     = curl_exec($ch);
